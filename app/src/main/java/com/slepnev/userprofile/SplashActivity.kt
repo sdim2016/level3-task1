65db4905a58fcd1bd9e96263fd552e27e5227f88
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-02-09 14:25:00 +0100

    Implemented the SplashActivity functionality

--- app/src/main/java/com/slepnev/userprofile/SplashActivity.kt
+++ app/src/main/java/com/slepnev/userprofile/SplashActivity.kt
@@ -1,12 +1,23 @@
 package com.slepnev.userprofile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(
+                Intent(
+                    this@SplashActivity, CreateProfileActivtiy::java
+                )
+            )
+            finish()
+        }, 1000)
     }
 }
