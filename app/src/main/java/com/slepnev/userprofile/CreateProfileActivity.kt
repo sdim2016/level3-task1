8be8bbe7191d686f63757a572baadad0536175b9
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-02-09 14:55:48 +0100

    Added the intent to open the image Gallery

--- app/src/main/java/com/slepnev/userprofile/CreateProfileActivity.kt
+++ app/src/main/java/com/slepnev/userprofile/CreateProfileActivity.kt
@@ -1,12 +1,30 @@
 package com.slepnev.userprofile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_create_profile.*
+
+const val GALLERY_REQUEST_CODE = 100
+
 
 class CreateProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_profile)
+        initViews()
+    }
+
+    private fun initViews() {
+        btnGallery.setOnClickListener {
+            onGalleryClick()
+        }
+    }
+
+    private fun onGalleryClick() {
+        val galleryIntent = Intent(Intent.ACTION_PICK)
+        galleryIntent.type = "image/*"
+        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE)
     }
 }
