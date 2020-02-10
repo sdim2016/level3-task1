156f5dc570a66dacbde3f7cc3d738cfb1d231665
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-02-10 12:59:37 +0100

    Finished the Profile activity

--- app/src/main/java/com/slepnev/userprofile/ProfileActivity.kt
+++ app/src/main/java/com/slepnev/userprofile/ProfileActivity.kt
@@ -2,11 +2,27 @@ package com.slepnev.userprofile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        initViews()
+    }
+
+    private fun initViews() {
+        val profile = intent.getParcelableExtra<Profile>(PROFILE_EXTRA)
+
+        if (profile != null) {
+            tvName.text = getString(R.string.name, profile.firstName, profile.lastName)
+            tvDescription.text = profile.description
+            if (profile.imageUri != null) ivProfileImage.setImageURI(profile.imageUri)
+        }
+    }
+
+    companion object {
+        const val PROFILE_EXTRA = "PROFILE_EXTRA"
     }
 }
